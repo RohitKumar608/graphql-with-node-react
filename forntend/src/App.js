13e1ba116c918d030e04c185bd3e871884c78655
rohitkumarm <rohitkumarrk13568@gmail.com>
2020-07-07 08:15:42 +0530

    added navigation in react app

--- forntend/src/App.js
+++ forntend/src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
+import MainNavigation from './components/Navigation/MainNavigation';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Graphql</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
